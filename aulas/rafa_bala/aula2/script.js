95262fa4d38f5517b116961e178fa1f8594a4380
RicoVianna <rluivianna@hotmail.com>
2025-03-05 23:06:40 -0300

    Atualizei aula 2

--- aulas/rafa_bala/aula2/script.js
+++ aulas/rafa_bala/aula2/script.js
@@ -1,19 +1,21 @@
 function adicionarTarefa() {
 
-    var mensagem = "Tarefa adicionada com sucesso!" //salva a mensagem para exibi-la quando a tarefa for adicionada 
+    const inputTarefa = document.getElementById("inputTarefa") 
+    var tarefa = inputTarefa.value.trim()
 
-    var inputTarefa = document.getElementById("inputTarefa") //procura a variável que tem o id "inputTarefa"
+    const mensagem = document.getElementById("mensagem")
 
-    var tarefa = inputTarefa.value //O valor que for digitado fica salvo na variável "tarefa"
+    if (tarefa == "") {
+        var mensagemErro = "Digite uma tarefa para adicioná-la a sua lista!"
+        mensagem.textContent = mensagemErro 
+    } else {
+        var mensagemSucesso = "Tarefa adicionada com sucesso!"
+        mensagem.textContent = mensagemSucesso
 
-    document.getElementById("mensagem").textContent = mensagem //Exibe a mensagem de confirmação, encontrando o elemento com o id "mensagem"
-    
-    var listaTarefas = document.getElementById("listaTarefas") //Encontra o elemento com o id "listaTarefas"
-    var novaTarefa = document.createElement("li") //Cria um novo item de lista (li)para adicionar à lista de tarefas
-
-    novaTarefa.textContent = tarefa //Atribui o texto digitado pelo usuário ao novo itel de lista
-    
-    listaTarefas.appendChild(novaTarefa) //Adiciona o novo item de tarefa à lista existente   
-
-    inputTarefa.value = "" //limpa o campo de entrada, deixando-o vazio após a tarefa ser adicionada
+        const listaTarefas = document.getElementById("listaTarefas") 
+        var novaTarefa = document.createElement("li") 
+        novaTarefa.textContent = tarefa     
+        listaTarefas.appendChild(novaTarefa) 
+    }
+    inputTarefa.value = "" 
 }

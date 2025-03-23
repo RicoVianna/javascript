2666ab555ce7a8711a67e0a87c0cdb46b5665be4
RicoVianna <rluivianna@hotmail.com>
2025-03-23 20:15:15 -0300

    Exercício de condições parte 1

--- aulas/cev/aula011/ex008.js
+++ aulas/cev/aula011/ex008.js
@@ -1,7 +1,6 @@
-var vel = 78.5
-console.log(`A velocidade do carro é ${vel} km/h`)
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel} km/h`)
 if (vel > 60) {
     console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
-}
-
+} 
 console.log(`Dirija sempre usando o cinto de segurança!`)

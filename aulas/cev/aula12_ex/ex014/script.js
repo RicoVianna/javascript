99a3df810c95c70ab8252b82bc5e5825d6ca2b96
RicoVianna <rluivianna@hotmail.com>
2025-04-14 21:18:50 -0300

    Alterei o ex012.ex do cev

--- aulas/cev/aula12_ex/ex014/script.js
+++ aulas/cev/aula12_ex/ex014/script.js
@@ -1,26 +1,23 @@
 function carregar() {
-    var msg = window.document.getElementById('msg');
-    var img = window.document.getElementById('imagem');
-    var data = new Date();
-    var hora = data.getHours();
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = 24 //data.getHours()
 
-    
-    
     msg.innerHTML = `Agora são ${hora} horas.`
 
-    if (hora >= 0 && hora < 12) {
-       //Bom dia!
-        img.src = 'imagens/manha.png'
-        document.body.style.background = '#e8e96d'
-    } else if (hora >= 12 && hora < 18) {
-        //Boa tarde
-        img.src = 'imagens/tarde.png'
-        document.body.style.background = '#cb7373'
+    if(hora >= 0 && hora < 12) {
+        //bom dia
+        img.src = 'amanhecer.png'
+        document.body.style.background = '#e2cd9f' //Encontra o body (parte que quer alterar sem precisar criar um id ou class para o elemento). Basta digitar "document." seguido do elemento que quer alterar, depois é só alterar pelo "style"
+    } else if(hora >=12 && hora < 18) {
+        //boa tarde
+        img.src = 'entardecer.png'
+        document.body.style.background = '#b9846f'
     } else {
-        //Boa noite
-        img.src = 'imagens/noite.png'
-        document.body.style.background = '#0a0a49'
+        //boa noite
+        img.src = 'anoitecer.png'
+        document.body.style.background = '#515154'
     }
+
 }
-
-

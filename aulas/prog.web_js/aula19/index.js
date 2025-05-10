6055093192b42bed7fd888f13567a98540e8f5e4
RicoVianna <rluivianna@hotmail.com>
2025-05-10 12:28:58 -0300

    atualizei curso prog web

--- aulas/prog.web_js/aula19/index.js
+++ aulas/prog.web_js/aula19/index.js
@@ -9,3 +9,50 @@ Para trabalhar com JSON no JavaScript, usamos dois métodos:
 1 -> JSON.parse() -> Converte texto no padrão JSON em objetos;
 2 -> JSON.stringfy() -> Converte objetos em textos padrão JSON
 */
+
+/*const carro = {
+    marca: 'Fiat',
+    modelo: 'Uno',
+    ano: 2001
+}
+
+let texto = JSON.stringify(carro);//convete o objeto para texto
+document.getElementById('area').innerHTML = texto;//imprime o texto convertido no HTML
+
+let objeto = JSON.parse(texto);//converte o texto para objeto
+console.log(objeto.modelo); //pegamos o valor deste objeto (no console.log)
+*/
+
+/*
+const carro = {
+    marca: 'Fiat',
+    modelo: 'Uno',
+    motor: ['1.6', '1.4', '1.0']
+}
+
+let texto = JSON.stringify(carro);
+document.getElementById('area').innerHTML = texto;
+
+let objeto = JSON.parse(texto);
+console.log(objeto.motor[0]); 
+*/
+
+function buscarCep() {
+    let input = document.getElementById('cep').value;
+    const ajax = new XMLHttpRequest();
+    ajax.open('GET', 'https://viacep.com.br/ws/' + input + '/json/')
+    ajax.send();
+
+    ajax.onload = function() {
+        document.getElementById('texto').innerHTML = this.responseText;
+        //transforma o texto em objeto(abaixo)
+        let objeto = JSON.parse(this.responseText);
+
+        //pega os valores desejados(abaixo)
+        let logradouro = objeto.logradouro;
+        let cidade = objeto.localidade;
+        let estado = objeto.uf;
+        document.getElementById('texto').innerHTML = 'Logradouro: ' +  logradouro + '<br> Cidade: ' + cidade + '<br> Estado: ' + estado;
+    }
+}
+

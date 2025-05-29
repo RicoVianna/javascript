e6cd44cc68d4691adeb3066935b69b37fd0ba28e
RicoVianna <rluivianna@hotmail.com>
2025-05-29 20:20:04 -0300

    Atualizei CFB js

---
+++ aulas/cfb_js/aula010/aula010.js
@@ -0,0 +1,14 @@
+let st = "I";
+
+
+res = (st == "A" ? "Ativo" : "Inativo");
+console.log(res);
+
+//teste lógico (num%2)
+//Operação (?) - se verdadeiro / se falso
+
+// 0 = false
+// 1 = true
+//true retorna o primeiro valor (par)
+//false retorna o segundo valor (impar)
+

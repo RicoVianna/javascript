2f96fa1d2cc657b2d9334adf87557ed39b5d0d92
RicoVianna <rluivianna@hotmail.com>
2025-05-29 13:56:28 -0300

    Atualizei CFB js

---
+++ aulas/cfb_js/aula008/aula008.js
@@ -0,0 +1,26 @@
+/*
+let n1 = 10;
+let n2 = 11;
+
+let res = n1 | n2;
+
+console.log(res);
+*/
+
+/*
+let n1 = 13;
+let n2 = 14;
+
+let res = n1 ^ n2;
+
+console.log(res);
+*/
+
+let n1 = 20;
+
+let res = n1 << 1;
+
+console.log(res);
+
+
+

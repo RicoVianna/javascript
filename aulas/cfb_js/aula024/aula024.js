8199368e20777450d910f5a03a7b565409bc991f
RicoVianna <rluivianna@hotmail.com>
2025-06-02 20:17:47 -0300

    Atualizei CFB js

---
+++ aulas/cfb_js/aula024/aula024.js
@@ -0,0 +1,15 @@
+/*
+const f = function(...valores) {
+    let res = 0;
+    for (v of valores) {
+        res += v;
+    }
+    return res;
+}
+console.log(f(10,5));
+*/
+
+const f = new Function('v1', 'v2', 'v3', 'return v1 + v2 +v3')//função construtor (anonima)
+    
+
+console.log(f(10, 5, 15));
---
+++ aulas/cfb_js/aula025/aula025.js
@@ -0,0 +1,22 @@
+/*
+const soma = function(v1, v2) {
+    return v1 + v2;
+}
+*/
+
+const soma = (v1, v2) => {
+    let res
+    return v1 + v2
+}
+console.log(soma(10, 5));
+
+const nome = n => {
+    return nome
+}
+console.log('Bruno')
+
+const add = n => n+10;
+console.log(add(10));
+
+
+

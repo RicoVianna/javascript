5acffeb605cf9f2cec46b28c945a5625d8878f69
RicoVianna <rluivianna@hotmail.com>
2025-06-03 16:21:53 -0300

    Atualizei CFB js

---
+++ aulas/cfb_js/aula028/aula028.js
@@ -0,0 +1,38 @@
+// const cursos = ['HTML', 'CSS', 'JavaScript', 'PHP', 'React'];
+// cursos.map((el, i) => {
+//     console.log('Curso: ' + el + ' - Posição do curso: ' + i + '!')
+// });
+
+// const cursos = ['HTML', 'CSS', 'JavaScript', 'PHP', 'React'];
+// let c = cursos.map((el, i) => {
+//     return el;
+// });
+// console.log(c);
+
+// const cursos = ['HTML', 'CSS', 'JavaScript', 'PHP', 'React'];
+// let c = cursos.map((el, i) => {
+//     return el;
+// });
+// console.log(c);
+
+// let el = 
+// el = [...el];
+// console.log(el);
+// el.map((e, i) => {
+//     e.innerHTML = 'CFB Cursos'
+    
+// });
+
+// const el = document.getElementsByTagName('div');
+// const val = Array.prototype.map.call(el,({innerHTML}) => innerHTML);
+// console.log(val);
+
+const converterInt = (e) => parseInt(e);
+const dobrar = (e) => e * 2;
+let num = ['1', '2', '3', '4', '5'].map(dobrar);
+console.log(num);
+
+
+
+
+
